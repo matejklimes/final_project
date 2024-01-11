bdfee16779b6e7f395dd35313262c08689e111e4
Matěj Klimeš <it2007@sspu-opava.cz>
2024-01-11 21:27:57 +0100

    Edited styles at workout list

--- gym_app - kopie/gymapp/gym/forms.py
+++ gym_app - kopie/gymapp/gym/forms.py
@@ -1,6 +1,7 @@
 # forms.py
 from django import forms
 from .models import Workout
+from datetime import datetime
 
 # forms.py
 from django import forms
@@ -9,6 +10,22 @@ class WorkoutForm(forms.ModelForm):
     class Meta:
         model = Workout
         fields = ['name', 'exercises']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Get the current time
+        current_time = datetime.now().time()
+
+        # Set default workout name based on the current time
+        if current_time >= datetime.strptime('05:00:00', '%H:%M:%S').time() and current_time <= datetime.strptime('11:59:59', '%H:%M:%S').time():
+            self.fields['name'].initial = 'Morning Workout'
+        elif current_time >= datetime.strptime('12:00:00', '%H:%M:%S').time() and current_time <= datetime.strptime('17:59:59', '%H:%M:%S').time():
+            self.fields['name'].initial = 'Afternoon Workout'
+        elif current_time >= datetime.strptime('18:00:00', '%H:%M:%S').time() and current_time <= datetime.strptime('22:59:59', '%H:%M:%S').time():
+            self.fields['name'].initial = 'Evening Workout'
+        else:
+            self.fields['name'].initial = 'Night Workout'
 
 
 
