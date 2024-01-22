1de59c916b3fa98a2a377c544bd033b6b2d7c38d
Matěj Klimeš <it2007@sspu-opava.cz>
2024-01-22 12:51:04 +0100

    Delete gym_app - kopie directory

--- gym_app - kopie/gymapp/gym/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Exercise, Workout, Set, WorkoutExercise
-
-admin.site.register(Exercise)
-admin.site.register(Workout)
-admin.site.register(WorkoutExercise)
-admin.site.register(Set)
